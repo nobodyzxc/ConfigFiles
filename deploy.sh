1d87433aa453e4f5d598e85352b20230a4683e03
nobodyzxc <nobodyzxc.tw@gmail.com>
2021-04-06 18:35:20 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,19 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-ln -s "$SCRIPTPATH/alias.sh"           ~/.alias.sh
-ln -s "$SCRIPTPATH/antergos.bashrc.sh" ~/.bashrc
-ln -s "$SCRIPTPATH/inputrc"            ~/.inputrc
-ln -s "$SCRIPTPATH/tmux.conf"          ~/.tmux.conf
-ln -s "$SCRIPTPATH/zshrc"              ~/.zshrc
-ln -s "$SCRIPTPATH/screenrc"           ~/.screenrc
-ln -s "$SCRIPTPATH/ghci"               ~/.ghci
-ln -s "$SCRIPTPATH/gitconfig"          ~/.gitconfig
+ln -sf "$SCRIPTPATH/alias.sh"           ~/.alias.sh
+ln -sf "$SCRIPTPATH/antergos.bashrc.sh" ~/.bashrc
+ln -sf "$SCRIPTPATH/inputrc"            ~/.inputrc
+ln -sf "$SCRIPTPATH/tmux.conf"          ~/.tmux.conf
+ln -sf "$SCRIPTPATH/zshrc"              ~/.zshrc
+ln -sf "$SCRIPTPATH/screenrc"           ~/.screenrc
+ln -sf "$SCRIPTPATH/ghci"               ~/.ghci
+ln -sf "$SCRIPTPATH/gitconfig"          ~/.gitconfig
 
-echo "source $SCRIPTPATH/vimrc.vim"      >> ~/.vimrc
-echo "source $SCRIPTPATH/extensions.vim" >> ~/.vimrc
+#echo "source $SCRIPTPATH/vimrc.vim"      >> ~/.vimrc
+#echo "source $SCRIPTPATH/extensions.vim" >> ~/.vimrc
 
 mkdir --parents ~/.config/termite
-ln -s "$SCRIPTPATH/termite.config"      ~/.config/termite/config
+ln -sf "$SCRIPTPATH/termite.config"      ~/.config/termite/config
 mkdir --parents ~/.config/terminator
-ln -s "$SCRIPTPATH/terminator.config"   ~/.config/terminator/config
+ln -sf "$SCRIPTPATH/terminator.config"   ~/.config/terminator/config
 mkdir --parents ~/.config/i3
-ln -s "$SCRIPTPATH/i3.conf"             ~/.config/i3/config
+ln -sf "$SCRIPTPATH/i3.conf"             ~/.config/i3/config
